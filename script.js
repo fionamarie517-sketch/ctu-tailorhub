c1c0e4c17f6ca3eb16adb991b29d3e7f98b25e6c
fionamarie517-sketch <fionamarie517@gmail.com>
2026-08-25 21:24:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -77,3 +77,14 @@ function showUniform(type) {
     }
 
 }
+
+const heroSlides = document.querySelectorAll('.hero-slide');
+let currentHeroSlide = 0;
+
+if (heroSlides.length > 0) {
+    setInterval(() => {
+        heroSlides[currentHeroSlide].classList.remove('active');
+        currentHeroSlide = (currentHeroSlide + 1) % heroSlides.length;
+        heroSlides[currentHeroSlide].classList.add('active');
+    }, 5000);
+}
